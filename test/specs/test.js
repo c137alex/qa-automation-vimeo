5faf5a8d4fca48cd70914bda6268fd6379631a72
camerajunkie <ebenofthecamera@gmail.com>
2022-06-13 09:08:34 -0400

    Reverting as per request of technical interviewer

--- test/specs/test.js
+++ test/specs/test.js
@@ -8,22 +8,5 @@ describe('My test application', () => {
 
         //await MainPage.dropDownTextFinder();
         await MainPage.dropDownMenuByText();
-
-        // going above and beyond
-        // await $('#input-email').waitForExist({ timeout: 15000 });
-        await MainPage.emailAddressInputField.waitForExist({ timeout: 15000 });
-        expect(await MainPage.emailAddressInputField).toBeSelected( { wait: 60000} );
-        console.log("derp derp");
-        //expect(await MainPage.passwordInputField).toBeDisplayed( { wait: 60000} );
-
-        await MainPage.emailInput();
-        //await MainPage.emailAddressInputField.setValue('blah@blah.com');
-        await MainPage.passwordInput();
-        await MainPage.clickCheckMeOutCheckbox();
-        await MainPage.clickSubmitButton();
-        console.log("post derp derp");
-        await MainPage.delayedElement1();
-        await MainPage.delayedElement2();
-        console.log("post post derp derp");
     });
 });
